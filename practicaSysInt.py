0219a6f8067d58ef3bb9db662511e4e480ac045a
FenriRuiz <32452140+FenriRuiz@users.noreply.github.com>
2018-09-26 00:38:52 +0200

    Eliminada la ruta del archivo

--- practicaSysInt.py
+++ practicaSysInt.py
@@ -1,5 +1,5 @@
 import networkx as nx 
-
+import sys
 '''FUNCIONES'''
 def perteneceNodo(osmidnodo):
 	for id, osmid in nodosEx.items():
@@ -9,7 +9,7 @@ def perteneceNodo(osmidnodo):
 
 '''CREACION DEL GRAFO Y POBLACIÓN DEL MISMO'''
 g = nx.Graph()
-g = nx.read_graphml("C://Users//fernando//Documents//SysInt//Migueltura.graphml")
+g = nx.read_graphml("Migueltura.graphml")
 
 ''' OBTENEMOS LOS NODOS EXISTENTES'''
 nodosEx = nx.get_node_attributes(g, 'osmid')
