edec0c7cfc9914594405df56fbe006775a8df7ce
FenriRuiz <32452140+FenriRuiz@users.noreply.github.com>
2018-09-25 22:39:21 +0200

    Función perteneceNodo
    Comprueba de los nodos existentes (nodosEx) si alguno de ellos se llama como el que estamos buscando, si encuentra alguno igual devolverá un True en caso contario un False

---
+++ practicaSysInt.py
@@ -0,0 +1,21 @@
+import networkx as nx 
+
+'''FUNCIONES'''
+def perteneceNodo(osmidnodo):
+	for id, osmid in nodosEx.items():
+		if osmid == osmidnodo:
+			return True
+	return False
+
+'''CREACION DEL GRAFO Y POBLACIÓN DEL MISMO'''
+g = nx.Graph()
+g = nx.read_graphml("C://Users//fernando//Documents//SysInt//Migueltura.graphml")
+
+''' OBTENEMOS LOS NODOS EXISTENTES'''
+nodosEx = nx.get_node_attributes(g, 'osmid')
+
+'''EJEMPLO OSMID EXISTENTE: 847673135''' 
+osmid = "847673135"
+print(perteneceNodo(osmid))
+
+
