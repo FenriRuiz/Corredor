e25cae131425811ec474571e9efdea0a6d462026
alejandroVerod <alejandro.velasco@alu.uclm.es>
2018-09-26 19:05:54 +0200

    Cambio en PerteneceNodo()
    He minimizado el metodo perteneceNodo ()

--- practicaSysInt.py
+++ practicaSysInt.py
@@ -7,15 +7,12 @@ class grafo():
 		self.graph=nx.read_graphml(file)
 		self.nodes=nx.get_node_attributes(self.graph,'osmid')
 	def perteneceNodo(self,osmid_node):
-		for id,osmid in self.nodes.items():
-			if osmid == osmid_node:
-				return True
-		return False
-
+		return osmid_node in self.nodes
 osmid = "847673135"
 file="Migueltura.graphml"
 g=grafo(file)
 print(g.perteneceNodo(osmid))
+
 
 
 
