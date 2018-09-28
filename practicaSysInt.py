686d98604f66018ac5cf649c8c523211a9c1959f
FenriRuiz <32452140+FenriRuiz@users.noreply.github.com>
2018-09-28 19:02:45 +0200

    posicionNodo implementada
    Pasandole un nodo, comprueba si existe el nodo, y si existe después busca la posición en ambos diccionarios, sería conveniente, encontrar un algoritmo que use otros metodos para aumentar la eficiencia.

--- practicaSysInt.py
+++ practicaSysInt.py
@@ -1,10 +1,24 @@
 import networkx as nx 
 '''FUNCIONES :)'''
 def perteneceNodo(osmidnodo):
-	for id, osmid in nodosEx.items():
+	for id, osmid in nodosOsmid.items():
 		if osmid == osmidnodo:
 			return True
 	return False
+def posicionNodo(osmidnodo):
+	if perteneceNodo(osmidnodo):
+		for id, x in nodosLat.items():
+			pos[0] = float(x)
+		for id, y in nodosLon.items():
+			pos[1] = float(y)
+		return pos
+	else:
+		raise ValueError("No existe el nodo seleccionado")
+
+def adyacentesNodo(osmidnodo):
+	it = nx.all_neighbors(g, osmid)
+	aristas = edges_iter()
+	return it
 
 '''CREACION DEL GRAFO Y POBLACIÓN DEL MISMO'''
 g = nx.Graph()
@@ -12,10 +26,13 @@ g = nx.Graph()
 g= nx.read_graphml("Migueltura.graphml")
 
 ''' OBTENEMOS LOS NODOS EXISTENTES'''
-nodosEx = nx.get_node_attributes(g, 'osmid')
+nodosOsmid = nx.get_node_attributes(g, 'osmid')
+nodosLat = nx.get_node_attributes(g, 'x')
+nodosLon = nx.get_node_attributes(g, 'y')
 
 '''EJEMPLO OSMID EXISTENTE: 847673135''' 
 osmid = "847673135"
 print(perteneceNodo(osmid))
-
-
+pos=[0,0]
+print(posicionNodo(osmid))
+adyacentesNodo(osmid)
