13bdbd8582aa3e9f9379a907ac62d74b52d5f031
alejandroVerod <alejandro.velasco@alu.uclm.es>
2018-09-29 14:01:21 +0200

    Método posicionNodo
    Según lo que he podido ver, el grafo tiene un montón de atributos, entre ellos _node que te da la información de todos los nodos en forma de un array bidimensional, en la primera dimensión se encuentran los osmid (los id de los nodos) y por cada uno te da la lontitud y latitud correspondiente :)

--- practicaSysInt.py
+++ practicaSysInt.py
@@ -8,10 +8,15 @@ class grafo():
 		self.nodes=nx.get_node_attributes(self.graph,'osmid')
 	def perteneceNodo(self,osmid_node):
 		return osmid_node in self.nodes
+	def posicionNodo(self,osmid_node):
+		posicion=[]
+		posicion.append(self.graph._node[osmid_node])
+		return posicion
 osmid = "847673135"
 file="Migueltura.graphml"
 g=grafo(file)
 print(g.perteneceNodo(osmid))
+print(g.posicionNodo(osmid))
 
 
 
