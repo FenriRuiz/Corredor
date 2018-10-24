e6b385e06e12d17380d1aded691c072f5589ea48
alejandroVerod <alejandro.velasco@alu.uclm.es>
2018-10-24 21:52:25 +0200

    Incluido grafo y explicacion de espacio estados

--- tarea2.py
+++ tarea2.py
@@ -1,6 +1,42 @@
 import networkx as nx 
 import hashlib
 import json
+##
+##TAREA 1
+class grafo():
+	graph=nx.Graph()
+	nodes=None
+	def __init__(self,file):
+		self.graph=nx.read_graphml(file)
+		self.nodes=self.graph._node
+	def perteneceNodo(self,osmid_node):
+		return osmid_node in self.nodes
+	def posicionNodo(self,osmid_node):
+		if(self.perteneceNodo(osmid_node)):
+			posicion=[]
+			posicion.append(self.nodes[osmid_node])
+			return posicion
+		else:
+			return "Error, el nodo no existe"
+	def adyacentesNodo(self,nodo_inicial):
+		listaAristas=[]
+		if(self.perteneceNodo(nodo_inicial)):
+			for nodo_final in self.graph.edges._adjdict[nodo_inicial]:
+				try:
+					nombre= self.graph.edges._adjdict[nodo_inicial][nodo_final][0]['name']
+				except KeyError:
+					nombre='SinNombre'
+				arista=({'nInicial' : nodo_inicial,'nFinal' : nodo_final, 'nombre' : nombre, 
+				'longitud' : self.graph.edges._adjdict[nodo_inicial][nodo_final][0]['length']})
+				listaAristas.append(arista)
+				'''Cambiar el name y ver si podemos devolverlo como una cuadrupleta'''
+		else:
+			print("Error, el nodo no existe")
+			return listaAristas
+		return listaAristas
+##
+
+
 class Estado:
     def __init__(self,nodoOSM):
         self.nodoActual=nodoOSM['node']
@@ -19,12 +55,14 @@ class Estado:
 # es=estado(problema['IntSt'])
 # print('')
 class EspacioEstados:
-    graph=nx.Graph()
-    sucesiones=[]
+    
     def __init__(self,file):
-        self.graph=nx.read_graphml(file)
+        self.graph=grafo(file)
+        self.listaEstados=[]
     def Sucesores(self,estado):
         for nodoDestino in estado.nodosPendientes:
+            #Hay que hacer funcion recursiva que llame a Sucesores y pille los adyacentes del nodo de estado y los añada
+            #a listaEstados
             accion="Estoy en "+estado.identificador+" y voy a "+nodoDestino
             sucesion=(accion,estado(nodoDestino),1)
             #Hace falta calcular el coste
